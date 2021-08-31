1284264af1505700f7e73bb14db119563bf14510
SimonT <simon.taschler@infominds.eu>
2021-08-31 11:16:19 +0200

    Disabling Type column for procedures & defines

--- XSharpPowerTools/View/Windows/CodeBrowserWindow.xaml.cs
+++ XSharpPowerTools/View/Windows/CodeBrowserWindow.xaml.cs
@@ -43,16 +43,20 @@ namespace XSharpPowerTools.View.Windows
 
         private void SetTableColumns(XSModelResultType resultType)
         {
-            ResultsDataGrid.Columns[0].Visibility = resultType == XSModelResultType.Member
-                ? Visibility.Visible
-                : Visibility.Collapsed;
+            ResultsDataGrid.Columns[0].Visibility = resultType == XSModelResultType.Type
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+
+            ResultsDataGrid.Columns[1].Visibility = resultType == XSModelResultType.Procedure
+                ? Visibility.Collapsed
+                : Visibility.Visible;
 
             ResultsDataGrid.Columns[0].Width = 0;
             ResultsDataGrid.Columns[1].Width = 0;
             ResultsDataGrid.Columns[2].Width = 0;
             ResultsDataGrid.Columns[3].Width = 0;
             ResultsDataGrid.UpdateLayout();
-            ResultsDataGrid.Columns[0].Width = new DataGridLength(4, DataGridLengthUnitType.Star);
+            ResultsDataGrid.Columns[0].Width = new DataGridLength(3, DataGridLengthUnitType.Star);
             ResultsDataGrid.Columns[1].Width = new DataGridLength(4, DataGridLengthUnitType.Star);
             ResultsDataGrid.Columns[2].Width = new DataGridLength(1, DataGridLengthUnitType.SizeToCells);
             ResultsDataGrid.Columns[3].Width = new DataGridLength(9, DataGridLengthUnitType.Star);
